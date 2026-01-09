2961b6a9a4da18f4a759ae14e935bc7268b4c03e
TXLuong <tranxuanluong98@gmail.com>
2026-01-09 16:13:42 +0700

    Update learning resource

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Design-pattern-golang
 This is for understanding practical design patterns used in golang
 
-Original resource: [LINK](https://www.youtube.com/watch?v=7yOORwOx5V8&list=PLOsM_3jFFQRmNCt68hxCdxi8i_fUx2wTZ&index=2)
+Original resource:
+- [LINK](https://www.youtube.com/watch?v=7yOORwOx5V8&list=PLOsM_3jFFQRmNCt68hxCdxi8i_fUx2wTZ&index=2)
+- [12-advanced-feature](https://dev.to/conquerym/deep-dive-into-go-exploring-12-advanced-features-for-building-high-performance-concurrent-applications-3i23)
+- [golang-deep-dive](https://nghiant3223.github.io/2025/04/15/go-scheduler.html#handling-system-calls)
--- creational/factory-method/problem/notification_service.go
+++ creational/factory-method/problem/notification_service.go
@@ -1,15 +1,17 @@
 package main
 
-type NotificationService struct {}
+type NotificationService struct {
+	name string
+}
 
 // Notification Service will depend on concrete Notifier 
 // When we have new notification method -> we need to change in this function -> violate OPEN/CLOSE principal 
-// 
-func (n NotificationService) SendNotification(notificationType string, content string) {
-	if notificationType == "email" {
+func (n *NotificationService) SendNotification(notificationType string, content string) {
+	switch notificationType {
+	case "email":
 		email := EmailNotification{}
 		email.Send(content)
-	} else if notificationType == "sms" {
+	case "sms":
 		sms := SmsNotification{}
 		sms.Send(content)
 	}
